37a054f6a448d8d9b5b565b03d20c92820094e7c
Ruslan Popov <ruslan_popov_2014@inbox.ru>
2023-12-20 18:14:01 +0300

    init commit

--- src/Arraylist/Affairs.java
+++ src/Arraylist/Affairs.java
@@ -1,2 +1,24 @@
-package Arraylist;public class Affairs {
+package Arraylist;
+
+
+// Класс нужен просто как сущность, у заметок есть только название (name)
+// Добавил конструктор и toString
+public class Affairs {
+
+    private String name;
+
+    public Affairs(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Affairs{" +
+               "name='" + name + '\'' +
+               '}';
+    }
+
 }
+
+
+
--- src/Arraylist/Main.java
+++ src/Arraylist/Main.java
@@ -1,7 +1,70 @@
+package Arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        // Реализация Arraylist.
+        // Создал несколько элементов в коллекцию экземпляра класса Affairs
+        // Создал 2 метода, для добавления новой заметки и для удаления любой заметки по её индексу
+        // Реализовал с использованием обоих методов, так же есть метод для вывода коллекции с её индексами.
 
+        Affairs affairs1 = new Affairs("Купить молоко");
+        Affairs affairs2 = new Affairs("Зайти на почту");
+        Affairs affairs3 = new Affairs("Приготовить ужин");
+        Affairs affairs4 = new Affairs("Поставить будильник");
 
+        List<Affairs> list = new ArrayList<>();
+
+        list.add(affairs1);
+        list.add(affairs2);
+        list.add(affairs3);
+        list.add(affairs4);
+
+        addAffairs(list);
+        deleteAffairs(list);
+
+    }
+
+    public static Scanner scanner;
+
+    // Метод для добавления новой заметки в коллекцию
+    public static List<Affairs> addAffairs(List<Affairs> list) {
+        scanner = new Scanner(System.in);
+        System.out.println("Добавьте новую заметку");
+        String name = scanner.nextLine();
+        Affairs affairs = new Affairs(name);
+        list.add(affairs);
+        System.out.println("Заметка успешно добавлена");
+        demonstrationList(list);
+        return list;
+    }
+
+    // Метод для удаления замметки по индексу
+    public static List<Affairs> deleteAffairs(List<Affairs> list) {
+
+        scanner = new Scanner(System.in);
+        System.out.println("Введите номер заметки, которую хотите удалить");
+        int index = scanner.nextInt();
+        if (index < 0 || index > list.size()) {
+            System.out.println("Такой заметки нет");
+        } else {
+            list.remove(index);
+            System.out.println("Заметка успешно удалена");
+        }
+        demonstrationList(list);
+        return list;
+    }
+
+    // Метод для вывода коллекции с индексами элементов
+    public static void demonstrationList(List<Affairs> list) {
+        int i = 0;
+        for (Affairs affairs : list) {
+            System.out.println(i + ": " + affairs);
+            i++;
+        }
     }
 }
